69741bd61ded72c65535a9e54147ed0c2203da9a
Gemi Khong <nhatvankhong@gmail.com>
2020-08-29 21:06:54 -0500

    Add Beer Controller Test. Close #4

---
+++ mssc-beer-service/src/test/java/guruspringframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,56 @@
+package guruspringframework.msscbeerservice.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guruspringframework.msscbeerservice.web.model.BeerDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void getBeerById() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveNewBeer() throws Exception {
+
+        BeerDTO beerDTO = BeerDTO.builder().build();
+        String beerDTOJson = objectMapper.writeValueAsString(beerDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/beer/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDTOJson))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void updateBeerById() throws Exception {
+
+        BeerDTO beerDTO = BeerDTO.builder().build();
+        String beerDTOJson = objectMapper.writeValueAsString(beerDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/beer/" + UUID.randomUUID().toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDTOJson))
+                .andExpect(status().isNoContent());
+    }
+}
