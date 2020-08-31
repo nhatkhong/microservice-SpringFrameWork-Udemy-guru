1ad6cf59e5a751622ead671eebf81fb9ad952e26
Gemi Khong <nhatvankhong@gmail.com>
2020-08-31 18:24:27 -0500

    Add @Validated and other annotations to validate inputs for methods in BeerController

--- mssc-brewery/src/main/java/guruspringframework/msscbrewery/web/controller/v2/BeerControllerv2.java
+++ mssc-brewery/src/main/java/guruspringframework/msscbrewery/web/controller/v2/BeerControllerv2.java
@@ -5,14 +5,17 @@ import guruspringframework.msscbrewery.web.model.v2.BeerDTOv2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Validated
 @RequestMapping("/api/v2/beer")
 @RestController
 public class BeerControllerv2 {
@@ -24,13 +27,13 @@ public class BeerControllerv2 {
     }
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDTOv2> getBeer(@PathVariable("beerId") UUID beerId) {
+    public ResponseEntity<BeerDTOv2> getBeer(@NotNull @PathVariable("beerId") UUID beerId) {
 
         return new ResponseEntity<>(beerServicev2.getBeerById(beerId), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity handlePost(@Valid @RequestBody BeerDTOv2 beerDTOv2) {
+    public ResponseEntity handlePost(@Valid @NotNull @RequestBody BeerDTOv2 beerDTOv2) {
 
         BeerDTOv2 savedDTO = beerServicev2.saveBeer(beerDTOv2);
 
