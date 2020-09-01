43756f8b41e761c31fd2de07577371c5e56c7811
Gemi Khong <nhatvankhong@gmail.com>
2020-08-31 23:59:43 -0500

    Fixing falling tests

--- mssc-beer-service/src/test/java/guruspringframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ mssc-beer-service/src/test/java/guruspringframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -2,6 +2,7 @@ package guruspringframework.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import guruspringframework.msscbeerservice.web.model.BeerDTO;
+import guruspringframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -9,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,7 +35,7 @@ class BeerControllerTest {
     @Test
     void saveNewBeer() throws Exception {
 
-        BeerDTO beerDTO = BeerDTO.builder().build();
+        BeerDTO beerDTO = getValidBeerDTO();
         String beerDTOJson = objectMapper.writeValueAsString(beerDTO);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/beer/")
@@ -45,12 +47,21 @@ class BeerControllerTest {
     @Test
     void updateBeerById() throws Exception {
 
-        BeerDTO beerDTO = BeerDTO.builder().build();
+        BeerDTO beerDTO = getValidBeerDTO();
         String beerDTOJson = objectMapper.writeValueAsString(beerDTO);
 
         mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/beer/" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDTOJson))
                 .andExpect(status().isNoContent());
+    }
+
+    BeerDTO getValidBeerDTO() {
+        return BeerDTO.builder()
+                .beerName("Valid Beer")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal("9.98"))
+                .upc(123455980584L)
+                .build();
     }
 }
